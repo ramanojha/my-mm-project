9260f317bc308607debba3cbca56419da9033e90
Raman Ojha <ojha.raman99@gmail.com>
2020-07-16 14:51:33 +0530

    Added TestNg Annotation

--- mm-project-two/src/test/java/com/test/project/setup/project/two/test/AppTestTwo.java
+++ mm-project-two/src/test/java/com/test/project/setup/project/two/test/AppTestTwo.java
@@ -2,12 +2,14 @@ package com.test.project.setup.project.two.test;
 
 
 
+import org.testng.annotations.Test;
+
 import com.test.project.setup.core.application.Application;
 import com.test.project.setup.project.two.src.AppTwo;
 
 public class AppTestTwo {
 	
-	
+	@Test
 	public static void appTestTwoTest(){
 		AppTwo.appTestTwoTest();
 		Application.appCodeMethodOne("Two");
